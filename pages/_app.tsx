4ae51b52931c1564eea8e6b278d40050bf429b38
malcodeman <26983729+malcodeman@users.noreply.github.com>
2021-10-31 13:11:49 +0100

    Adds fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
+import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import * as Fathom from "fathom-client";
 
 import theme from "../theme";
 
+const FATHOM_SITE_ID = "DYETUSIG";
+
 function App({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    Fathom.load(FATHOM_SITE_ID, {
+      url: "https://warbler.resumebuilder.dev/script.js",
+      includedDomains: ["malcodeman.com", "www.malcodeman.com"],
+    });
+  }, []);
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
