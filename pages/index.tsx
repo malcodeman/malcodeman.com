d9afacff4e8c21d809bcf9ed6a809a901cb543fc
malcodeman <26983729+malcodeman@users.noreply.github.com>
2021-10-31 14:54:24 +0100

    Adds document title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import { Box, Flex, Heading, Stack, Link, Text } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
 import { Container, Grid } from "@chakra-ui/react";
@@ -105,75 +106,80 @@ type props = {
 function Home(props: props) {
   const { pinnedItems } = props;
   return (
-    <Container maxWidth="container.xl">
-      <Grid gridTemplateColumns={{ lg: "1fr 1fr" }} gridGap={{ lg: 6 }}>
-        <Flex
-          paddingY="6"
-          flexDir="column"
-          justifyContent="space-between"
-          top={{ lg: "0" }}
-          position={{ lg: "sticky" }}
-          height={{ lg: "100vh" }}
-        >
-          <Box>
-            <Heading mb="2">Hello, I’m Amer Karamustafić.</Heading>
-            <Text mb="4">
-              I’m full-stack engineer from Sarajevo, Bosnia and Herzegovina.
-            </Text>
+    <>
+      <Head>
+        <title>malcodeman.com</title>
+      </Head>
+      <Container maxWidth="container.xl">
+        <Grid gridTemplateColumns={{ lg: "1fr 1fr" }} gridGap={{ lg: 6 }}>
+          <Flex
+            paddingY="6"
+            flexDir="column"
+            justifyContent="space-between"
+            top={{ lg: "0" }}
+            position={{ lg: "sticky" }}
+            height={{ lg: "100vh" }}
+          >
+            <Box>
+              <Heading mb="2">Hello, I’m Amer Karamustafić.</Heading>
+              <Text mb="4">
+                I’m full-stack engineer from Sarajevo, Bosnia and Herzegovina.
+              </Text>
+            </Box>
+            <Box>
+              <Stack direction={["column", "row"]} spacing="2">
+                {LINKS.map((item) => (
+                  <Link key={item.url} href={item.url} isExternal>
+                    <Button
+                      href={item.url}
+                      leftIcon={item.icon}
+                      mr="2"
+                      rightIcon={<ExternalLink size={16} />}
+                    >
+                      {item.text}
+                    </Button>
+                  </Link>
+                ))}
+              </Stack>
+            </Box>
+          </Flex>
+          <Box paddingY="6">
+            <Box as="section" marginBottom="6">
+              <Heading mb="4" textTransform="uppercase" fontSize="md">
+                Projects
+              </Heading>
+              <Grid gridGap="4" gridTemplateColumns={{ xl: "1fr 1fr" }}>
+                {pinnedItems.user.pinnedItems.edges.map((item) => (
+                  <Project
+                    key={item.node.id}
+                    url={item.node.url}
+                    language={item.node.languages.edges[0]?.node.name}
+                    name={item.node.name}
+                    description={item.node.description}
+                  />
+                ))}
+              </Grid>
+            </Box>
+            <Box as="section">
+              <Heading mb="4" textTransform="uppercase" fontSize="md">
+                Websites
+              </Heading>
+              <Grid gridGap="4" gridTemplateColumns={{ xl: "1fr 1fr" }}>
+                {WEBSITES.map((item) => (
+                  <Project
+                    key={item.url}
+                    url={item.url}
+                    language=""
+                    name={item.name}
+                    description={item.description}
+                  />
+                ))}
+              </Grid>
+            </Box>
           </Box>
-          <Box>
-            <Stack direction={["column", "row"]} spacing="2">
-              {LINKS.map((item) => (
-                <Link key={item.url} href={item.url} isExternal>
-                  <Button
-                    href={item.url}
-                    leftIcon={item.icon}
-                    mr="2"
-                    rightIcon={<ExternalLink size={16} />}
-                  >
-                    {item.text}
-                  </Button>
-                </Link>
-              ))}
-            </Stack>
-          </Box>
-        </Flex>
-        <Box paddingY="6">
-          <Box as="section" marginBottom="6">
-            <Heading mb="4" textTransform="uppercase" fontSize="md">
-              Projects
-            </Heading>
-            <Grid gridGap="4" gridTemplateColumns={{ xl: "1fr 1fr" }}>
-              {pinnedItems.user.pinnedItems.edges.map((item) => (
-                <Project
-                  key={item.node.id}
-                  url={item.node.url}
-                  language={item.node.languages.edges[0]?.node.name}
-                  name={item.node.name}
-                  description={item.node.description}
-                />
-              ))}
-            </Grid>
-          </Box>
-          <Box as="section">
-            <Heading mb="4" textTransform="uppercase" fontSize="md">
-              Websites
-            </Heading>
-            <Grid gridGap="4" gridTemplateColumns={{ xl: "1fr 1fr" }}>
-              {WEBSITES.map((item) => (
-                <Project
-                  key={item.url}
-                  url={item.url}
-                  language=""
-                  name={item.name}
-                  description={item.description}
-                />
-              ))}
-            </Grid>
-          </Box>
-        </Box>
-      </Grid>
-    </Container>
+        </Grid>
+      </Container>
+    </>
   );
 }
 
