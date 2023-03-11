26b0bec52488bbcb1f6e435f9434fed1492904b7
malcodeman <26983729+malcodeman@users.noreply.github.com>
2023-03-11 21:47:31 +0100

    Removes unused code

--- types.ts
+++
@@ -1,8 +0,0 @@
-import { ColorMode } from "@chakra-ui/color-mode";
-
-type ChakraThemeConfig = {
-  initialColorMode: ColorMode;
-  useSystemColorMode: boolean;
-};
-
-export type { ChakraThemeConfig };
