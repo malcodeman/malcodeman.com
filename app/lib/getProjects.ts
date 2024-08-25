9dda2f988d83cf3054bd56b50a26d4b07f3bac69
malcodeman <26983729+malcodeman@users.noreply.github.com>
2024-08-25 22:11:03 +0200

    feat: updates project list

--- app/lib/getProjects.ts
+++ app/lib/getProjects.ts
@@ -22,25 +22,25 @@ export function getProjects(variant: Variant) {
         width: 1059,
       },
     },
-    {
-      url: "https://www.bosnianswho.design",
-      title: "bosnianswho.design",
-      description:
-        "A directory of inspiring Bosnians and Herzegovinians in the IT industry.",
-      bgColor: "#D9AD7780",
-      picture: [
-        {
-          srcSet: `/bosnians_who_design_macbook_${variant}.webp`,
-          media: "(min-width: 1024px)",
-          height: 2520,
-          width: 4072,
-        },
-      ],
-      image: {
-        src: `/bosnians_who_design_iphone_${variant}.webp`,
-        height: 2028,
-        width: 1059,
-      },
-    },
+    // {
+    //   url: "https://www.bosnianswho.design",
+    //   title: "bosnianswho.design",
+    //   description:
+    //     "A directory of inspiring Bosnians and Herzegovinians in the IT industry.",
+    //   bgColor: "#D9AD7780",
+    //   picture: [
+    //     {
+    //       srcSet: `/bosnians_who_design_macbook_${variant}.webp`,
+    //       media: "(min-width: 1024px)",
+    //       height: 2520,
+    //       width: 4072,
+    //     },
+    //   ],
+    //   image: {
+    //     src: `/bosnians_who_design_iphone_${variant}.webp`,
+    //     height: 2028,
+    //     width: 1059,
+    //   },
+    // },
   ];
 }
