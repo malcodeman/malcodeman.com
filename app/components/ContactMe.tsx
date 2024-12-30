6d4baa9fe240674ef034388e0708488eb179d392
Amer K <26983729+malcodeman@users.noreply.github.com>
2024-12-30 15:24:31 +0100

    feat: adds react icons (#11)

--- app/components/ContactMe.tsx
+++ app/components/ContactMe.tsx
@@ -1,25 +1,24 @@
-import { siGithub, siDevdotto, siDribbble } from "simple-icons";
-import { MailIcon } from "lucide-react";
+import { LuMail } from "react-icons/lu";
+import { SiGithub, SiDevdotto, SiDribbble } from "react-icons/si";
 import { Button } from "@/ui/Button";
 import { IconButton } from "@/ui/IconButton";
 import { cn } from "@/lib/utils";
-import SimpleIcon from "./SimpleIcon";
 
 const LINKS = [
   {
     label: "Github",
     url: "https://github.com/malcodeman",
-    icon: <SimpleIcon size={16} path={siGithub.path} />,
+    icon: <SiGithub />,
   },
   {
     label: "Dev.to",
     url: "https://dev.to/malcodeman",
-    icon: <SimpleIcon size={16} path={siDevdotto.path} />,
+    icon: <SiDevdotto />,
   },
   {
     label: "Dribbble",
     url: "https://dribbble.com/malcodeman",
-    icon: <SimpleIcon size={16} path={siDribbble.path} />,
+    icon: <SiDribbble />,
   },
 ];
 
@@ -28,7 +27,7 @@ function ContactMe() {
     <div className="flex flex-col sm:flex-row sm:items-center">
       <a href="mailto:malcodeman@gmail.com" className="mb-6 sm:mb-0 sm:mr-6">
         <Button className="w-full hover:scale-x-110 hover:scale-y-110">
-          <MailIcon size={16} />
+          <LuMail />
           Contact me
         </Button>
       </a>
